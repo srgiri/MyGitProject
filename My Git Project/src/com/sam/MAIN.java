56b8f32abc3888698c0c738f66bfe8c08c60fc92
Saumya Ranjan Giri <saumya.giri07@gmail.com>
2018-03-19 21:07:34 +0530

    Second Commit

--- My Git Project/src/com/sam/MAIN.java
+++ My Git Project/src/com/sam/MAIN.java
@@ -5,6 +5,7 @@ public class MAIN {
 	public static void main(String[] args) {
 		System.out.println("Hello git");
 		System.out.println("First Modification");
+		System.out.println("Second Modification");
 	}
 
 }
